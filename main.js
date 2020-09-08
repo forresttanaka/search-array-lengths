58e4b1f670900c6c2111d1b350b50d0f3997008b
Forrest Tanaka <forrest@forrest-tanaka.com>
2020-09-08 07:08:53 -0700

    Update for full functionality

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const cliProgress = require('cli-progress');
 
+/** Number of objects to get in one chunk */
+const CHUNK_SIZE = 500;
+
 /**
  * Convert a user key and secret assigned to them on an encoded site to an authorization string for
  * XHR requests.
@@ -48,6 +51,14 @@ const readKeyfile = async (keyfile) => {
     return JSON.parse(results);
 };
 
+const encodedURIComponent = value => (
+    encodeURIComponent(value)
+        .replace(/\(/g, '%28')
+        .replace(/\)/g, '%29')
+        .replace(/%3A/g, ':')
+        .replace(/%20/g, '+')
+);
+
 /**
  * Create a new cart on the specified host.
  * @param {string} host URL of host to perform search on
@@ -57,24 +68,20 @@ const readKeyfile = async (keyfile) => {
  *
  * @return {Promise} Search result object
  */
-const newCart = (host, auth, suffix, debug) => {
+const getChunk = (host, auth, type, field, from, debug) => {
     if (debug) {
-        console.log('NEW CART REQUEST %s:%s', host, suffix);
+        console.log('REQUEST %s:%s:%s', host, type, field);
     }
-    return fetch(`${host}/carts/@@put-cart`, {
-        method: 'PUT',
+    return fetch(`${host}/report/?type=${type}&limit=${CHUNK_SIZE}&from=${from}&field=${encodedURIComponent(field)}`, {
+        method: 'GET',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
             Authorization: auth,
         },
-        body: JSON.stringify({
-            name: `Test Cart ${suffix}`,
-            locked: false,
-        }),
     }).then((response) => {
         if (debug) {
-            console.log('NEW CART RESPONSE %o', response);
+            console.log('SEARCH RESPONSE %o', response);
         }
 
         // Convert response to JSON
@@ -88,23 +95,39 @@ const newCart = (host, auth, suffix, debug) => {
 };
 
 /**
- * Create multiple new empty carts.
- * @param {number} count Number of new carts to create
- * @param {number} start Starting suffix number for cart name "Test Cart {number}"
- * @param {string} host URL of host on which to create carts
- * @param {string} auth base64-encoded key and secret for POST permission
- * @param {object} progressBar Instance of progress-bar object
- * @param {bool} debug True to output debug messages to console
+ * Extract the value of an object property based on a dotted-notation field,
+ * e.g. { a: 1, b: { c: 5 }} you could retrieve the 5 by passing 'b.c' in `field`.
+ * Based on https://stackoverflow.com/questions/6393943/convert-javascript-string-in-dot-notation-into-an-object-reference#answer-6394168
+ * @param {object} object Object containing the value you want to extract.
+ * @param {string} field  Dotted notation for the property to extract.
  *
- * @return {Promise} Promise for cart creation
+ * @return {value} Whatever value the dotted notation specifies, or undefined.
  */
-const multipleNewCarts = async (count, start, host, auth, progressBar, debug) => {
-    for (let i = 0; i < count; i += 1) {
-        const response = await newCart(host, auth, i + parseInt(start, 10), debug);
-        if (debug) {
-            console.log('NEW CART JSON %o', response);
+const getObjectFieldValue = (object, field) => {
+    const parts = field.split('.');
+    if (parts.length === 1) {
+        return object[field];
+    }
+    return parts.reduce((partObject, part) => partObject && partObject[part], object);
+};
+
+const getAllChunks = async (host, auth, type, field, less, greater, debug) => {
+    let from = 0;
+    let chunksEnded = false;
+    const results = [];
+    while (!chunksEnded) {
+        const chunk = await getChunk(host, auth, type, field, from, debug);
+        chunksEnded = chunk['@graph'].length === 0;
+        if (!chunksEnded) {
+            chunk['@graph'].forEach((obj) => {
+                const lengthField = field.substring(0, field.lastIndexOf('.'));
+                const value = getObjectFieldValue(obj, lengthField);
+                if (value && value.length >= greater && value.length <= less) {
+                    console.log('%s - %s', obj['@id'], value.length);
+                }
+            });
+            from += CHUNK_SIZE;
         }
-        progressBar.update(i + 1);
     }
 };
 
@@ -112,8 +135,10 @@ program
     .version('1.0.0')
     .option('-k, --key [key]', 'key of keyfile', 'localhost')
     .option('-f, --keyfile [filename]', 'keyfile name/path', 'keypairs.json')
-    .option('-c, --count [number]', 'number of carts to create', '1')
-    .option('-s, --start [number]', 'starting cart number', '1')
+    .option('-t, --type [string]', 'type of objects to search', 'Experiment')
+    .option('-p, --property [string]', 'property to search', 'files.@id')
+    .option('-g, --greater [string]', 'filter to array lengths >= this', '1')
+    .option('-l, --less [string]', 'filter to array lengths <= this', Number.MAX_VALUE)
     .option('-d, --debug', 'Debug flag', false)
     .parse(process.argv);
 
@@ -122,9 +147,7 @@ const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_cla
 
 readKeyfile(program.keyfile).then((resultJson) => {
     keyFileData = resultJson;
-    progressBar.start(program.count, 0);
     const auth = keypairToAuth(keyFileData[program.key].key, keyFileData[program.key].secret);
-    return multipleNewCarts(program.count, program.start, keyFileData[program.key].server, auth, progressBar, program.debug);
-}).then(() => {
-    progressBar.stop();
+    return getAllChunks(keyFileData[program.key].server, auth, program.type, program.property, program.less, program.greater, program.debug);
+}).then((searchResults) => {
 });
